0d96358131d45ef74ea7b33ae03712dad745e52f
NicoPaez <nicopaez@gmail.com>
2019-06-01 10:01:40 -0300

    Fixes and new cases added

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+base_url=http://requestbin.fullcontact.com/14i1x111/ api_token=mytoken mvn clean verify -Pwiki
--- src/main/java/guarabot/JsonFixture.java
+++ src/main/java/guarabot/JsonFixture.java
@@ -2,18 +2,37 @@ package guarabot;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
 
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JsonFixture {
 
-    protected String numeroOrigen;
-    protected String numeroDestino;
-    protected String fechaHoraInicio;
-    protected String fechaHoraFin;
+    protected HttpClient client;
+    protected HttpResponse response;
+    protected static final String RESET_PATH = "reset";
+    protected String targetUrl;
+    protected String apiToken;
 
-    protected String materia;
+    protected String nombreMateria;
     protected String codigoMateria;
     protected String docente;
     protected int cupo;
@@ -27,7 +46,7 @@ public class JsonFixture {
     protected String prepararRequestAltaMateria() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Map<String,Object> data =  new HashMap<String, Object>();
-        data.put("materia", this.materia);
+        data.put("nombreMateria", this.nombreMateria);
         data.put("codigo", this.codigoMateria);
         data.put("docente", this.docente);
         data.put("cupo", this.cupo);
@@ -52,4 +71,77 @@ public class JsonFixture {
       data.put("notas", this.notas);
       return mapper.writeValueAsString(data);
   }
+
+
+    protected boolean submitPost(String path, String body) throws IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        this.client = createHttpClient();
+        System.out.println("target url:" + this.targetUrl);
+        HttpPost request = new HttpPost(this.targetUrl + path);
+        request.addHeader(this.getTokenHeader());
+        HttpEntity entity = new StringEntity(body, ContentType.APPLICATION_JSON);
+        request.setEntity(entity);
+        this.response = client.execute(request);
+        if (this.response == null) return false;
+        return true;
+    }
+
+    protected Header getTokenHeader() {
+        return new Header() {
+            @Override
+            public String getName() {
+                return "api_token";
+            }
+
+            @Override
+            public String getValue() {
+                return apiToken;
+            }
+
+            @Override
+            public HeaderElement[] getElements() throws ParseException {
+                return new HeaderElement[0];
+            }
+        };
+    }
+
+    protected HttpClient createHttpClient() {
+        SSLContext sslContext = null;
+        try {
+            sslContext = new SSLContextBuilder()
+                    .loadTrustMaterial(null, (x509CertChain, authType) -> true)
+                    .build();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+        return HttpClientBuilder.create()
+                .setSslcontext(sslContext)
+                .setConnectionManager(
+                        new PoolingHttpClientConnectionManager(
+                                RegistryBuilder.<ConnectionSocketFactory>create()
+                                        .register("http", PlainConnectionSocketFactory.INSTANCE)
+                                        .register("https", new SSLConnectionSocketFactory(sslContext,
+                                                NoopHostnameVerifier.INSTANCE))
+                                        .build()
+                        ))
+                .build();
+    }
+
+
+    public void reset(){
+        HttpPost request = new HttpPost(this.targetUrl + RESET_PATH);
+        HttpEntity entity = new StringEntity("", ContentType.APPLICATION_JSON);
+        request.setEntity(entity);
+        try {
+            HttpResponse response = client.execute(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
 }
